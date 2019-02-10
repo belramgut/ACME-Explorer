4e9b343a3063e08ce1cb8ed42bcd2b694cfb9337
Alejandro Hall <alejandro@geographica.gs>
2019-02-10 20:12:20 +0100

    Add finder model

---
+++ api/models/finderModel.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var FinderSchema = new Schema({
+    keyword: [{
+        type: String,
+        required: 'Kindly enter the keyword'
+    }],
+    lowerPrice: {
+        type: Number,
+        min: 0,
+    },
+    higherPrice: {
+        type: Number,
+        min: 0,
+    },
+    fromDate: {
+        type: Date,
+        required: 'Please, enter a start date for this finder'
+    },
+    toDate: {
+        type: Date,
+        required: 'Please, enter an end date for this finder'
+    },
+    explorer: {
+        type: Schema.Types.ObjectId,
+        ref: 'Explorer'
+    },
+
+}, { strict: false });
+
+
+module.exports = mongoose.model('Finder', FinderSchema);
