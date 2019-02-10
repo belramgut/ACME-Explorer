e1bdf698944936f8e4b1510b70dd6768c3eee662
Alejandro Hall <alejandro@geographica.gs>
2019-02-10 20:12:12 +0100

    Add application model

---
+++ api/models/applicationModel.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ApplicationSchema = new Schema({
+    applicationMoment: {
+        type: Date,
+        default: Date.now
+    },
+    status: [{
+        type: String,
+        required: 'Kindly enter the application status',
+        enum: ['PENDING', 'REJECTED', 'DUE', 'ACCEPTED', 'CANCELLED']
+    }],
+    comments: [String],
+    explorer: {
+        type: Schema.Types.ObjectId,
+        ref: 'Explorer'
+    },
+    trip: {
+        type: Schema.Types.ObjectId,
+        ref: 'Trip'
+    },
+
+}, { strict: false });
+
+
+module.exports = mongoose.model('Application', ApplicationSchema);
