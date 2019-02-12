ee4d0c8329ed02fae8300aa1faf589240ac7f596
Alejandro Hall <alejandro@geographica.gs>
2019-02-12 19:47:46 +0100

    Modify routers to match requirements

--- api/routes/finderRouter.js
+++ api/routes/finderRouter.js
@@ -3,12 +3,13 @@
 module.exports = function (app) {
     var finder = require('../controllers/finderController');
 
+    app.route('/v1/finders/search')
+        .get(finder.search_finders)
+
     app.route('/v1/finders')
-        .get(finder.list_all_finders)
         .post(finder.create_a_finder);
 
     app.route('/v1/finders/:finderId')
-        .get(finder.read_a_finder)
+        .delete(finder.delete_an_finder)
         .put(finder.update_a_finder)
-        .delete(finder.delete_an_finder);
 };
