d359fcca4e0bfb4ad926c58c7d58f517789d417b
Belen Ramos <belramgut@alum.us.es>
2019-02-14 13:47:01 +0100

    comments on routers

--- api/routes/tripRouter.js
+++ api/routes/tripRouter.js
@@ -3,17 +3,49 @@
 module.exports = function (app) {
     var trip = require('../controllers/tripController');
 
+    /**
+     * Get all trips
+     *      RequiredRoles: any
+     * Post a trip
+     *      RequiredRoles: Manager
+     * 
+     * @section trips
+     * @type get post
+     * @url /v1/trips
+     */
     app.route('/v1/trips')
         .get(trip.list_all_trips)
         .post(trip.create_a_trip);
-    
+
+    /**
+     * Search engine for trips
+     * Get trips depending on params
+     *      RequiredRoles: any
+     * 
+     * @section trips
+     * @type get
+     * @url /v1/trips/search
+     * @param {string} keyword
+     */
     app.route('/v1/trips/search')
         .get(trip.search_by_keyword);
-    
+
+    /**
+     * Get a specific trip
+     *      RequiredRoles: any
+     * Put a trip
+     *      RequiredRoles: be the manager who owns it
+     * Delete a trip
+     *      RequiredRoles: be the manager who owns it
+     * 
+     * @section trips
+     * @type get put delete
+     * @url /v1/trips/:tripId
+     */
     app.route('/v1/trips/:tripId')
         .get(trip.read_a_trip)
         .put(trip.update_a_trip)
         .delete(trip.delete_a_trip);
 
-    
+
 };
