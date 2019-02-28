e805152c679cbb36da53bea2834b319e22928d50
Isaamarod <belamarod@gmail.com>
2019-02-28 23:10:48 +0100

    changes

---
+++ api/controllers/dataWareHouseController.js
@@ -0,0 +1,7 @@
+
+var async = require("async");
+var mongoose = require('mongoose'),
+  DataWareHouse = mongoose.model('DataWareHouse'),
+  Trips =  mongoose.model('Trips');
+  Finder=  mongoose.model('Finder');
+
---
+++ api/routes/dataWareHouseRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var dataWareHouse = require('../controllers/dataWareHouseController');
+
+/*
+1. The customers who have placed more non-cancelled orders (top 10%).
+2. The customers who have placed less non-cancelled orders (bottom 10%).
+3. The clerks who have served and delivered more orders (top 10%).
+4. The clerks who have served and delivered fewer orders (bottom 10%).
+5. The ratio of orders that have been cancelled during the current month.
+6. The average price range that explorers indicate in their finders.
+7. The top 10 key words that the explorers indicate in their finders.
+*/
+
+};
