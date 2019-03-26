c7fd6f7a20b0d38860885a3c404ff4556c392768
Alejandro Hall <alejandro@geographica.gs>
2019-03-26 18:10:39 +0100

    Add role auth

--- api/routes/applicationRouter.js
+++ api/routes/applicationRouter.js
@@ -2,15 +2,24 @@
 
 module.exports = function (app) {
     var application = require('../controllers/applicationController');
+    var authController = require('../controllers/authController');
 
     app.route('/v1/applications/search')
         .get(application.search_applications);
 
     app.route('/v1/applications')
         .post(application.create_an_application);
+    
+    app.route('/v2/applications')
+        .post(authController.verifyUser(["EXPLORER"]),  application.create_an_application);
 
     app.route('/v1/applications/:applicationId')
         .get(application.read_an_application)
         .put(application.update_an_application)
         .delete(application.delete_an_application);
+    
+    app.route('/v2/applications/:applicationId')
+        .get(authController.verifyUser(["EXPLORER", "MANAGER"]), application.read_an_application)
+        .put(authController.verifyUser(["EXPLORER", "MANAGER"]), application.update_an_application)
+        .delete(authController.verifyUser(["EXPLORER", "MANAGER"]), application.delete_an_application);
 };
--- api/routes/finderRouter.js
+++ api/routes/finderRouter.js
@@ -2,6 +2,7 @@
 
 module.exports = function (app) {
     var finder = require('../controllers/finderController');
+    var authController = require('../controllers/authController');
 
     app.route('/v1/finders/search')
         .get(finder.search_finders);
@@ -15,4 +16,18 @@ module.exports = function (app) {
         
     app.route('/v1/finders/:finderId/apply_search')
         .get(finder.apply_search);
+    
+    
+    app.route('/v2/finders/search')
+        .get(authController.verifyUser(["EXPLORER"]),  finder.search_finders);
+
+    app.route('/v2/finders')
+        .post(authController.verifyUser(["EXPLORER"]),  finder.create_a_finder);
+
+    app.route('/v2/finders/:finderId')
+        .delete(authController.verifyUser(["EXPLORER"]),  finder.delete_an_finder)
+        .put(authController.verifyUser(["EXPLORER"]),  finder.update_a_finder);
+        
+    app.route('/v2/finders/:finderId/apply_search')
+        .get(authController.verifyUser(["EXPLORER"]),  finder.apply_search);
 };
